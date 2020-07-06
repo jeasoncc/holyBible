18308438cc01df75b4261d6f752e5febe7790757
jeasoncc <chenzhang7618@163.com>
2020-07-06 14:05:05 +0800

    sasa

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsModule } from './cats/cats.module';
 import { BookModule } from './book/book.module';
 
 @Module({
-  imports: [CatsModule, BookModule],
+  imports: [BookModule],
   controllers: [AppController],
   providers: [AppService],
 })
